78edf78512bcea509787778d68daef7ec330bd66
Matthew <savageguy@gmail.com>
2020-10-17 21:22:40 +1300

    Upped the custom resolution limit

--- src/renderer/views/Worker.vue
+++ src/renderer/views/Worker.vue
@@ -215,9 +215,9 @@ async function fullscreenScreenshot (callback) {
           chromeMediaSource: 'desktop',
           chromeMediaSourceId: 'window:' + windowID + ':0',
           minWidth: 1280,
-          maxWidth: 10000,
+          maxWidth: 20000,
           minHeight: 720,
-          maxHeight: 10000
+          maxHeight: 20000
         }
       }
     });
