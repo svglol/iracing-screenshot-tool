5493dec7411c90f05c2eea4e6056aa47bbd28f15
Matthew <savageguy@gmail.com>
2020-10-20 15:31:05 +1300

    Update Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,12 @@ module.exports = {
     "generator-star-spacing": "off",
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
   ignorePatterns: ["dist/*", "build/*"],
 };
